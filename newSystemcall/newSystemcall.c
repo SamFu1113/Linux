0a553d9295c518d441cc980d50d11bec519748e9
SamFu1113 <sam86111374@gmail.com>
2021-11-29 16:10:15 +0800

    Update newSystemcall.c
    Update newSystemcall.c.

--- newSystemcall/newSystemcall.c
+++ newSystemcall/newSystemcall.c
@@ -11,37 +11,59 @@ struct data_segment{
 };
 
 
-SYSCALL_DEFINE2(newSystemcall, pid_t, user_pid, void* __user, user_address){
+static int mpid=1;
+
+static void print_vma(struct task_struct *task)
+{
+        struct mm_struct *mm;
+        struct vm_area_struct *vma;
+        int count=0;
+        mm = task->mm;
+        printk("This mm_struct has %d vma\n", mm->map_count);
+// &i   （virtual add）
+// copy_from_user（&i）
+        for(vma = mm->mmap; vma; vma=vma->vm_next){
+                printk("vma number %d: \n", ++count);
+                // copy_to_user
+                printk("Start address 0x%lx, End address 0x%lx\n", vma->vm_start, vma->vm_end);
+                printk("Start physical address 0x%lx, End physical address 0x%lx\n", virt_to_phys(vma->vm_start), virt_to_phys(vma->vm_end));
+        }
+
+        /*
+        printk("Code segment start=0x%lx, end=0x%lx\n"
+                "Data Segment start=0x%lx, end=0x%lx\n"
+                "Stack segment start=0x%lx\n",
+                mm->start_code, mm->end_code, mm->start_data, mm->end_data, mm->start_stack
+                );
+        */
+}
+
+// asmlinkage long sys_newSystemcall(pid_t user_pid, void* __user user_address);
+// asmlinkage long my_get_physical_addresses(unsigned long* initial, int len_vir, unsigned int* result, int len_phy)
+
+SYSCALL_DEFINE2(newSystemcall, pid_t user_pid, unsigned long *initial){
 	struct task_struct *task_list;
 	struct data_segment my_data_segment;
 	int ret=0, count=0;
-	
+
 	printk("Hello, this is lab2. This lab will add a new system call.\n");
 	printk("Pid we want is: %d\n", user_pid);
-	for_each_process(task_list){
-		printk("Task list pid: %d\n", task_list->pid);
-		if(task_list->pid == user_pid){
-			printk("task pid==user pid\n");
-            my_data_segment.start_code = task_list->mm->start_code;
-			printk("after start\n");
-            my_data_segment.end_code = task_list->mm->end_code;
 
-            ret = copy_to_user(user_address, &my_data_segment, sizeof(struct data_segment));
-			if(ret!=0){
-				printk("Copy to user space failed!\n");
-			}
-        }
-		++count;
-		printk("Executed: %d times\n", count);
-		if(count==1000){
-			return 0;
-		}
+
+	//for_each_process(task_list) {
+		//if(task_list->pid == user_pid){
+	printk("%s[%d]\n", current->comm, current->pid);
+	print_vma(current);
+		
+
+	for(int i = 0; i< 3; ++i){
+		printk(virt_to_phys(initial[i]));
 	}
-	printk("end!\n");
-	printk("start code: %lx\n", my_data_segment.start_code);
-	printk("physical start code: %lx\n", virt_to_phys(my_data_segment.start_code));
-	printk("end code: %lx\n ", my_data_segment.end_code);
-    printk("physical end code: %lx\n", virt_to_phys(my_data_segment.end_code));
 
+	if(ret!=0){
+		printk("Copy to user space failed!\n");
+	}
+
+	printk("\n\n");
 	return 0;
 }
