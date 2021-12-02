6ee3383def45a76f2e0b28b034622b231bffc4c5
SamFu1113 <sam86111374@gmail.com>
2021-12-02 21:14:37 +0800

    Update lab2.c and add so file
    Update lab2.c and add so file.

--- lab2/lab2.c
+++ lab2/lab2.c
@@ -1,43 +1,77 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <pthread.h>
 #include <unistd.h>
 #include <syscall.h>
+#include <dlfcn.h>
 
 
+#define __NR_getPhyAddr 442
 
-#define __NR_newSystemcall 441
-
+/*
 struct data_segment{
-	unsigned long start_code;
+    unsigned long start_code;
     unsigned long end_code;
-    unsigned long test_1;
 };
+*/
 
-// 計數器
+// data
+static int data = 1;       // 儲存於 data 區段（可讀寫區域）
+//bss
+static int bss_address;           // 儲存於 bss 區段
+
+// TLS
 __thread int i = 0;
 
+unsigned long result[7];
+
+char result_name[7][100] = { "code", "data", "BSS", "heap", "libraries", "stack", "thread_local_storages"};
 // 子執行緒函數
 void* child() {
-    struct data_segment my_data_segment;
-    pid_t pid=getpid();
-    int a = syscall(__NR_newSystemcall, pid, (void*)&my_data_segment);
+    //library
+    
+    void *fHandle;
+    void (*func)();
+
+
+    fHandle = dlopen("./libso.so",RTLD_LAZY);
+    if (!fHandle) {
+        fprintf (stderr, "%s\n", dlerror());
+        exit(1);
+    }
+    dlerror();
+    func = (void(*)())dlsym(fHandle,"bejo_lib");
+    
+    //heap
+    char *heap_address=NULL;
+    heap_address = (char *) malloc(15);
+    
+    //stack
+    int stack_address=1;             // 儲存於 stack 區段
     i++;
-    if(a < 0)
-    {
+
+
+    unsigned long arr[7] = {&child, &data, &bss_address, heap_address, func, &stack_address, &i};
+    
+    int a = syscall(__NR_getPhyAddr, arr,7, result, 7);
+    
+    if(a < 0){
         perror("Sorry,Your system call appears to have failed.");
     }
-    else
-    {
-        printf("child i address %p val %d\n", &i, i);
-        //virt_to_phy(&i)
-        printf("Congratulations! Your system call is functional. Run the command dmesg in the terminal and find out!\n");
-        printf("Start: %lx\nEnd: %lx\n", my_data_segment.start_code, my_data_segment.end_code);
+    else{
+    	for(int i=0; i<7; ++i){
+    	    printf("virt %s : %#lx\n\n", result_name[i], arr[i]);
+    	    printf("phys %s : %#lx\n\n", result_name[i], result[i]);
+    	}
+    	printf("-------------------------------------\n");
     }
+    //free(heap_address);
     pthread_exit(NULL);
 }
 
 // 主程式
 int main() {
+
   pthread_t t1, t2, t3;
   pthread_create(&t1, NULL, child, NULL);
   pthread_create(&t2, NULL, child, NULL);
---
+++ lab2/libso.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int bejo_lib(char *name, int i)
+{
+    printf("I am %s, do i=%d\n", name, i);
+
+    return 0;
+}
